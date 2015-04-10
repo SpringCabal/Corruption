60a0ec430e5bdad388b727252f382e3908afd279
Eshed <ashdnazg@gmail.com>
2015-04-10 23:15:41 +0300

    engineer for scale

--- units/humans.lua
+++ units/humans.lua
@@ -6,7 +6,13 @@ local Dudette = BaseHuman:New{
 	name				= "PlaceHolderDudette",
 }
 
+local Engineer = BaseHuman:New{
+	name				= "PlaceHolderDudette",
+	objectName				= "<NAME>.s3o",
+}
+
 return lowerkeys({
 	Dude = Dude,
 	Dudette = Dudette,
+	Engineer = Engineer,
 })
